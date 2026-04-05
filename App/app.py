0351d59c38ab20f1e7bfdc1493d4b0ec0b6ea8ef
AmanTrivedi2007 <amantrivedi1107@gmail.com>
2026-04-05 16:30:45 +0530

    first version of the website

---
+++ App/app.py
@@ -0,0 +1,18 @@
+
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import StreamingResponse
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="../static"), name="static")
+templates = Jinja2Templates(directory="../templates")
+
+@app.get("/")
+def index(request:Request):
+    return templates.TemplateResponse(
+    request=request,
+    name="index.html"
+)
